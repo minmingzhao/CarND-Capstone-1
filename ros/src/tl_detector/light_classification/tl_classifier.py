e079037a9f0e80a85c41cebeda5d2da40b1720c8
9mat <dhlong.hp@gmail.com>
2017-10-14 00:04:13 +0800

    change order of classes and add some logging for debug

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -5,7 +5,7 @@ import numpy as np
 
 TLC_GRAPH_PATH = 'optimized_graph.pb'
 TLC_INPUT_SHAPE = (300, 400)
-TL_STATES = [TrafficLight.GREEN, TrafficLight.UNKNOWN, TrafficLight.RED, TrafficLight.YELLOW]
+TL_STATES = [TrafficLight.RED, TrafficLight.YELLOW, TrafficLight.GREEN, TrafficLight.UNKNOWN]
 
 class TLClassifier(object):
     def __init__(self):
@@ -21,7 +21,9 @@ class TLClassifier(object):
 
             self.input_tensor = self.tlc_graph.get_tensor_by_name('input_1:0')
             self.predictions_tensor = self.tlc_graph.get_tensor_by_name('predictions/Softmax:0')
-            self.sess = tf.Session(graph=tlc_graph)
+            self.sess = tf.Session(graph=self.tlc_graph)
+
+        self.img_count = 0
 
 
 
@@ -40,5 +42,13 @@ class TLClassifier(object):
         predictions = self.sess.run(self.predictions_tensor, {self.input_tensor: [image]})
 
         tl_state_id = np.argmax(predictions)
+
+        rospy.logwarn('Detect traffic light = %s', ['red', 'yellow', 'green', 'unknown'][tl_state_id])
+
+        self.img_count += 1
+
+        if self.img_count < 10:
+            scipy.misc.imsave('/home/dhlong/github/ros_img/ros_out/img'+ str(self.img_count) + '_state' + str(tl_state_id) + '.png', image)
+
 
         return TL_STATES[tl_state_id]
